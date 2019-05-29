0dd79fb89f1cd32b4e03787a2b429827a87796bb
Daniel Dyssegaard Kallick <ddyssegaardk@gmail.com>
2019-05-29 14:43:34 -0400

    whoops file

--- t.js
+++
@@ -1,7 +0,0 @@
-const { Document } = require('basichtml');
-global.document = new Document();
-const { Weaver, escapeHTML, unescapeHTML } = require('./index');
-const weaver = new Weaver();
-
-console.log('IN', weaver.encode('<b>\n</b>'));
-console.log('RT', weaver.decode(weaver.encode('<b>\n</b>')));
